b624fef6c9422cb8e45d81aa8cd6e5a8f11d46f4
Jeremy Scatigna <scatignaj@me.com>
2023-12-29 16:48:29 +0100

    feat(mail): remove comments

--- apps/web/app/(app)/mail/page.tsx
+++ apps/web/app/(app)/mail/page.tsx
@@ -2,15 +2,8 @@
 
 import { accounts } from "@/components/mail/data";
 import { Mail } from "@/components/mail/components/mail";
-// import { Filters, getFilterFunction } from "@/utils/filters";
-// import { usePromptContext } from "@/providers/PromptProvider";
 
 export default function MailPage() {
-  // const layout = cookies().get('react-resizable-panels:layout');
-  //   const collapsed = cookies().get('react-resizable-panels:collapsed');
-
-  //   const defaultLayout = layout ? JSON.parse(layout.value) : undefined;
-  //   const defaultCollapsed = collapsed ? JSON.parse(collapsed.value) : undefined;
   return (
     <Mail
       accounts={accounts}
